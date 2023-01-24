a189e826b6dcfda57629758c1032245b7d0c7e4b
TimoHenderson <tjhenderson2@gmail.com>
2023-01-24 19:18:59 +0000

    exercise_d completed

--- exercise_d_advanced_logic.py
+++ exercise_d_advanced_logic.py
@@ -3,19 +3,40 @@
 numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 
 # 1. Print out a list of the even integers:
-
+evens = []
+for number in numbers:
+    if number % 2 == 0:
+        evens.append(number)
+print(evens)
 
 # 2. Print the difference between the largest and smallest value:
-
+nums_copy = numbers.copy()
+nums_copy.sort()
+difference = nums_copy[-1] - nums_copy[0]
+print(difference)
 
 # 3. Print True if the list contains a 2 next to a 2 somewhere.
-
+prev_num = None
+for number in numbers:
+    if number == 2 and prev_num == 2:
+        print(True)
+        break
+    prev_num = number
 
 # 4. Print the sum of the numbers, 
 #    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
 #    
 #    So [11, 6, 4, 99, 7, 11] would have sum of 22
-
+sum = 0
+add = True
+for number in numbers:
+    if number != 6 and add:
+        sum += number
+    elif number == 6:
+        add = False
+    elif number == 7:
+        add = True
+print(sum)
 
 # 5. HARD! Print the sum of the numbers. 
 #    Except the number 13 is very unlucky, so it does not count.
@@ -24,8 +45,18 @@ numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 #
 #    So [5, 13, 2] would have sum of 5. 
 
+sum = 0
+add = True
 
+for number in numbers:
+    if number == 13:
+        add = False
+    elif add == False:
+        add = True
+    else:
+        sum += number
 
+print(sum)
 
 
 
